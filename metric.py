b44562b62c6375a769ae4685292a7a99b6d9ab54
문지환 <wlghks1215@gmail.com>
2019-11-27 12:33:28 +0900

    해시태그 예측에 맞게 precision, recall 수정

--- metric.py
+++ metric.py
@@ -1,34 +1,52 @@
 import numpy as np
 
-# precision is the specificity
-# "Specificity relates to the test's ability to correctly detect patients without a condition."
-def precision(actual, prediction):
-    fp = 0.
-    tp = 0.
-    for (index, val) in enumerate(actual):
-        for (i, v) in enumerate(val):
-            if (actual[index][i] == 1 and prediction[index][i] == 1):
-                tp += 1
-            elif (actual[index][i] == 0 and prediction[index][i] == 1):
-                fp += 1
-    #print("tp: " + str(tp))
-    #print("fp: " + str(fp))
-    return tp/(tp + fp)
 
-# recall is the sensitivity
-# "Sensitivity refers to the test's ability to correctly detect patients who do have the condition."
-def recall(actual, prediction):
-    fn = 0.
-    tp = 0.
-    for (index, val) in enumerate(actual):
-        for (i, v) in enumerate(val):
-            if (actual[index][i] == 1 and prediction[index][i] == 1):
-                tp += 1
-            elif (actual[index][i] == 1 and prediction[index][i] == 0):
-                fn += 1
-    #print("tp: " + str(tp))
-    #print("fn: " + str(fn))
-    return tp/(tp + fn)
+
+
+
+def precision(actual, prediction, start_index=2, end_index=3):
+    """
+    actual: 실제 해시태그 목록, <start>와 <end> 제외
+    prediction: 예측 해시태그 목록, <start>와 <end> 제외
+    """
+    
+    actual = set(actual)
+    prediction = set(prediction)
+    
+    actual.discard('<start>')
+    actual.discard('<end>')
+    actual.discard(start_index)
+    actual.discard(end_index)
+    
+    prediction.discard('<start>')
+    prediction.discard('<end>')
+    prediction.discard(start_index)
+    prediction.discard(end_index)
+    
+    return len(actual.intersection(prediction)) / len(prediction)
+
+
+def recall(actual, prediction, start_index=2, end_index=3):
+    """
+    actual: 실제 해시태그 목록, <start>와 <end> 제외
+    prediction: 예측 해시태그 목록, <start>와 <end> 제외
+    """
+    
+    actual = set(actual)
+    prediction = set(prediction)
+    
+    actual.discard('<start>')
+    actual.discard('<end>')
+    actual.discard(start_index)
+    actual.discard(end_index)
+    
+    prediction.discard('<start>')
+    prediction.discard('<end>')
+    prediction.discard(start_index)
+    prediction.discard(end_index)
+    
+    return len(actual.intersection(prediction)) / len(actual)
+
 
 def f1_score(actual, prediction):
     prec = precision(actual, prediction)
