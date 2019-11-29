f05ac9cf3ee57a5bc53e47d2e8ae8f052d4e7576
문지환 <wlghks1215@gmail.com>
2019-11-29 11:05:36 +0900

    precision, recall 인자 이름 수정

--- metric.py
+++ metric.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 
-def precision(actual, prediction, start_index=2, end_index=3):
+def precision(actual, prediction, start_token_index=2, end_token_index=3):
     """
     actual: 실제 해시태그 목록, <start>와 <end> 제외
     prediction: 예측 해시태그 목록, <start>와 <end> 제외
@@ -15,18 +15,18 @@ def precision(actual, prediction, start_index=2, end_index=3):
     
     actual.discard('<start>')
     actual.discard('<end>')
-    actual.discard(start_index)
-    actual.discard(end_index)
+    actual.discard(start_token_index)
+    actual.discard(end_token_index)
     
     prediction.discard('<start>')
     prediction.discard('<end>')
-    prediction.discard(start_index)
-    prediction.discard(end_index)
+    prediction.discard(start_token_index)
+    prediction.discard(end_token_index)
     
     return len(actual.intersection(prediction)) / len(prediction)
 
 
-def recall(actual, prediction, start_index=2, end_index=3):
+def recall(actual, prediction, start_token_index=2, end_token_index=3):
     """
     actual: 실제 해시태그 목록, <start>와 <end> 제외
     prediction: 예측 해시태그 목록, <start>와 <end> 제외
@@ -37,13 +37,13 @@ def recall(actual, prediction, start_index=2, end_index=3):
     
     actual.discard('<start>')
     actual.discard('<end>')
-    actual.discard(start_index)
-    actual.discard(end_index)
+    actual.discard(start_token_index)
+    actual.discard(end_token_index)
     
     prediction.discard('<start>')
     prediction.discard('<end>')
-    prediction.discard(start_index)
-    prediction.discard(end_index)
+    prediction.discard(start_token_index)
+    prediction.discard(end_token_index)
     
     return len(actual.intersection(prediction)) / len(actual)
 
